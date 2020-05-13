5a1c2759dbfe4f39312d5aed51272fada81f7914
Weebtor <48831156+Vistorete@users.noreply.github.com>
2020-05-13 00:30:25 -0400

    bromiun momentum
    bromun momemntum

--- app/routes/rutas_victor.py
+++ app/routes/rutas_victor.py
@@ -1,9 +1,44 @@
 from flask import Flask,Blueprint,render_template,request,redirect,url_for,flash,session,jsonify
 #from config import db,cursor
 import os,time,bcrypt
+import mysql.connector
 
-mod = Blueprint("rutas_victor",__name__)
+connection = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "sodasoda",
+    database = "LABEIT"
+)
+
+
+
+mod = Blueprint('rutas_victor',__name__)
+
+@mod.route('/victor/login')
+def prueba_login():
+    return render_template('victor/user_login_form.html')
+
 
 @mod.route("/victor",methods=["GET"])
 def principal():
-    return "OK"
+    select_query = "SELECT * from Usuario"
+    cursor = connection.cursor()
+    cursor.execute(select_query)
+    records = cursor.fetchall()
+    for i in range(len(records)):
+        print("records:", records[i][0])
+    return records[0][0]
+
+@mod.route('/victor/user_add_form')
+def add_user():
+    return render_template('victor/user_add_form.html')
+
+@mod.route('/victor/añadir/usuario', methods = ['POST'])
+def add_user2():
+    if request.method == 'POST':
+        rut = request.form['rut']
+        contraseña = request.form['contraseña']
+        email = request.form['email']
+        credencial = request.form['credencial']
+        print(rut, email, credencial)
+        return redirect('/')
