667f20891ab7d5dd4edcab705db35d5deaa77df6
Weebtor <48831156+Vistorete@users.noreply.github.com>
2020-06-01 22:21:03 -0400

    se agrego una puerta trasera
    borrar antes de producción

--- app/routes/rutas_victor.py
+++ app/routes/rutas_victor.py
@@ -13,11 +13,11 @@ def redirect_url(default='index'): # Redireccionamiento desde donde vino la requ
 mod = Blueprint('rutas_victor',__name__)
 
 @mod.route('/victor/login')
-def prueba_login():
+def validar_session():
 
     # Si hay una sesion entra al perfil
-    if 'rut' in session:
-        return redirect('/victor/perfil')
+    if 'usuario' in session:
+        return redirect('/perfil')
 
     # Si no hay sesion redirecciona al login
     else:
@@ -42,7 +42,6 @@ def login_session():
             FROM Usuario
             WHERE rut = %s;
         ''')
-        cursor = db.cursor()
         cursor.execute(query, (rut_entrada,))
         resultado = cursor.fetchall()
 
@@ -61,9 +60,10 @@ def login_session():
             return redirect('/victor/login')
 
 
-@mod.route('/victor/perfil')
+@mod.route('/perfil')
 def perfil():
-    if 'rut' in session:
+    # si hay un usuario en la session
+    if 'usuario' in session:
         query_perfil = ('''
             SELECT
                 id_credencial,
@@ -80,20 +80,46 @@ def perfil():
             FROM Usuario
             WHERE rut = %s
         ''')
-        cursor = db.cursor()
-        cursor.execute(query_perfil,(session['rut'],))
-        resultado_perfil = cursor.fetchall()
+        cursor.execute(query_perfil,(session['usuario']['rut'],))
+        resultado_perfil = cursor.fetchone()
+        for key in resultado_perfil:
+            print(resultado_perfil[key])
         print('resultado: ', resultado_perfil)
-        return render_template('victor/perfil.html', perfil_data = resultado_perfil[0])
-
+        return render_template('victor/perfil.html', perfil_info = resultado_perfil)
     else: 
-        return redirect('/victor/login')
+        return redirect('/victor/back_login') # ** cambiar url en produccion ** 
 
 
 
+# ********** borrar ante de producción ************************ #
+# puerta trasera de login 
+@mod.route('/victor/back_login')
+def back_login():
+    return render_template('victor/back_login.html')
 
+# Validador del login
+@mod.route('/victor/validar_back_login', methods = ['POST'])
+def validar_back_login():
+    datos_login=request.form.to_dict()
+    print(datos_login)
+    query = ('''
+            SELECT
+                id_credencial,
+                rut       
+            FROM Usuario
+            WHERE rut = %s
+        ''') % (datos_login['rut'])
+    cursor.execute(query)
+    resultado = cursor.fetchone()
+    print(resultado)
+    session['usuario'] = {}
+    session['usuario']['rut'] = resultado['rut']
+    session['usuario']['id_credencial'] = resultado ['id_credencial']
 
+    print('session:', session)
+    return redirect('/perfil')
 
+# ************************************************************** #
 
 # Formulario para agregar usuario
 @mod.route('/victor/user_add_form')
