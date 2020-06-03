b4ed4482f2cadaf7f84b814c48c8177427c5a4ca
SebaSwash <52325435+SebaSwash@users.noreply.github.com>
2020-06-03 10:35:10 -0400

    Update rut_chile.py

--- rut_chile.py
+++ rut_chile.py
@@ -7,4 +7,4 @@ def formato_rut(rut_entrada):
     elif len(rut_entrada) == 12:
         return ('{d[0]}{d[1]}{d[3]}{d[4]}{d[5]}{d[7]}{d[8]}{d[9]}{d[11]}'.format(d = rut_entrada))
     else:
-        return 'Rut invalido'
+        return 'Rut inválido'
