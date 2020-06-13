44a6d194c79b8f74b9a7fccafd66ce84d0cd66b2
Jun <coni.zv98@gmail.com>
2020-06-12 22:30:19 -0400

    actualice rutas, agregue ver_equipos

--- app/routes/rutas_cony.py
+++ app/routes/rutas_cony.py
@@ -9,6 +9,17 @@ def redirect_url(default='index'): # Redireccionamiento desde donde vino la requ
            request.referrer or \
            url_for(default)
 
-@mod.route("/solicitudes_prestamos",methods=["GET"])
+#@mod.route("/solicitudes_prestamos",methods=["GET"])
 def principal():
     return render_template('busqueda-solicitudes/test2.html')
+
+@mod.route("/solicitudes_prestamos",methods=["GET"])
+def ver_equipos():
+    if "equipo" not in session.keys():
+        return redirect("/")
+    query="""
+        SELECT Equipo.codigo AS codigo, Equipo.marca AS marca, Equipo.modelo AS modelo, Equipo.descripcion AS descripcion, Equipo.stock AS stock FROM Equipo
+          """
+    cursor.execute(query)
+    equipos=cursor.fetchall()
+    return render_template("/busqueda-solicitudes/ver_equipos.html",equipos=equipos)
