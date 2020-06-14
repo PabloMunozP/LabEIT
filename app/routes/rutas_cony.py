da8a25bac66abde7d215b927c59f05d9e24a0c56
Jun <coni.zv98@gmail.com>
2020-06-13 20:58:33 -0400

    upd rutas, busqueda

--- app/routes/rutas_cony.py
+++ app/routes/rutas_cony.py
@@ -17,7 +17,8 @@ def principal():
 def ver_equipos():
     if "usuario" not in session.keys():
         return redirect("/")
-    query="""
+    #Query para ver la lista de equipos
+    query_ver_equipos="""
         SELECT
             Equipo.id,
             Equipo.codigo,
@@ -36,7 +37,38 @@ def ver_equipos():
             Equipo.id = Etiqueta_equipo.id_equipo AND Etiqueta.id = Etiqueta_equipo.id_etiqueta
 
         GROUP BY Equipo.codigo
+
           """
-    cursor.execute(query)
+    cursor.execute(query_ver_equipos)
     equipos=cursor.fetchall()
     return render_template("/busqueda-solicitudes/busqueda.html",equipos=equipos)
+
+@mod.route("/solicitudes_prestamos/buscar",methods=["POST"])
+def buscar_equipo():
+    if 'usuario' not in session:
+        return redirect('/')
+    if request.method == 'POST':
+        busqueda = request.form.get('busqueda')
+        # Query para mostrar resultados de busqueda
+        query = '''
+            SELECT
+                Equipo.id,
+                Equipo.codigo,
+                Equipo.modelo,
+                Equipo.marca,
+                Equipo.descripcion,
+                Equipo.dias_max_prestamo,
+                Etiqueta.descripcion AS etiqueta,
+                COUNT(CASE WHEN Equipo_diferenciado.activo = 1 THEN 1 ELSE NULL END) AS disponibles,
+                COUNT(Equipo_diferenciado.activo) AS total_equipos
+            FROM
+                Equipo
+                LEFT JOIN Equipo_diferenciado ON Equipo_diferenciado.codigo_equipo = Equipo.codigo,
+                Etiqueta, Etiqueta_equipo
+            WHERE
+                Equipo.id = Etiqueta_equipo.id_equipo AND Etiqueta.id = Etiqueta_equipo.id_etiqueta AND (Equipo.modelo=busqueda OR Equipo.marca=busqueda OR Equipo.codigo=busqueda)
+
+            GROUP BY Equipo.codigo'''
+        cursor.execute(query)
+        resultados = cursor.fetchall()
+        return render_template('busqueda.html',equipos=resultados)
