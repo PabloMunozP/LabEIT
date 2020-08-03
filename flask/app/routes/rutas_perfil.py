c3ded101eba4830736d1651f1af099e51b9c00e7
Weebtor <48831156+Vistorete@users.noreply.github.com>
2020-08-03 10:39:51 -0400

    Update de perfil
    Se agregan 2 tablas y modal con una tabla

--- flask/app/routes/rutas_perfil.py
+++ flask/app/routes/rutas_perfil.py
@@ -13,48 +13,59 @@ def redirect_url(default='index'): # Redireccionamiento desde donde vino la requ
 mod = Blueprint('rutas_perfil',__name__)
 
 
-# Quuery de solicitudes de equipos del usuario
-def consultar_solicitudes_alumno_compuesta(rut_alumno):
+# Query de solicitudes de equipos del usuario
+def get_id_from_list_of_dictionary(list_of_dictionaries): # Funcion para almacenar las id de las solicitudes
+    result = []
+    for element in list_of_dictionaries:
+        result.append(element["id"])
+    return result
+
+def consultar_solicitudes_alumno(rut_alumno): # Query para poder consultar las solicitudes
     query = ('''
-            SELECT Detalle_solicitud.id_solicitud,
-                Solicitud.rut_alumno,    
-                Solicitud.rut_profesor,
-                Solicitud.motivo,
-                Solicitud.fecha_registro,
-                Detalle_solicitud.id_equipo,
-                Equipo.codigo,
-                Equipo.nombre,
-                Equipo.modelo,
-                Equipo.marca,
-                Estado_detalle_solicitud.nombre AS estado
+            SELECT Solicitud.id,
+                Usuario.nombres AS responsable_nombres,    
+                Usuario.apellidos AS responsable_apellidos,    
+                Usuario_profesor.nombres AS supervisor_responsable_nombres,
+                Usuario_profesor.apellidos AS supervisor_responsable_apellidos,
+                DATE(Solicitud.fecha_registro) AS fecha_registro
             FROM Solicitud
-            RIGHT JOIN Detalle_solicitud ON Detalle_solicitud.id_solicitud = Solicitud.id
-            LEFT JOIN Estado_detalle_solicitud ON Estado_detalle_solicitud.id = Detalle_solicitud.estado
-            LEFT JOIN Equipo ON Equipo.id = Detalle_solicitud.id_equipo 
-            
+            LEFT JOIN Usuario ON Usuario.rut = Solicitud.rut_alumno
+            LEFT JOIN Usuario AS Usuario_profesor ON Usuario_profesor.rut = Solicitud.rut_profesor
             WHERE Solicitud.rut_alumno = %s
         ''')
     cursor.execute(query,(rut_alumno,))
     query_solicitud = cursor.fetchall()
     return query_solicitud
 
+def consultar_equipos_solicitudes_alumno(list_id_solicitudes): # Query para consultar todos los equipos en relacion a las solicitudes
+    if len(list_id_solicitudes) < 1:
+        return []
+        
+    print(list_id_solicitudes)
+    format_strings = ','.join(['%s'] * len(list_id_solicitudes)) # Genera un string para la query
+    query = ('''
+        SELECT Detalle_solicitud.id_solicitud,
+            Detalle_solicitud.fecha_inicio,
+            Detalle_solicitud.fecha_termino,
+            Detalle_solicitud.fecha_devolucion,
+            Detalle_solicitud.fecha_vencimiento,
+            Equipo.codigo,
+            Equipo.nombre,
+            Equipo.modelo,
+            Equipo.marca,
+            Estado_detalle_solicitud.nombre AS estado
+        FROM Detalle_solicitud
+        LEFT JOIN Equipo ON Detalle_solicitud.id_equipo = Equipo.id
+        LEFT JOIN Estado_detalle_solicitud ON Estado_detalle_solicitud.id = Detalle_solicitud.estado
+        WHERE Detalle_solicitud.id_solicitud IN (%s)
+    ''' % format_strings)
+    cursor.execute(query,tuple(list_id_solicitudes))
+    query_resultado = cursor.fetchall()
+    return query_resultado
 
-@mod.route('/debug_sol_query') # link para hacer debug de la query
-def debug_sol_query():
-    solicitudes = consultar_solicitudes_alumno_compuesta("198182354")
-    for i in solicitudes:
-        print(i)
-    return 'debug_sol_query'
 
-
-@mod.route('/perfil') # ** Importante PERFIL** #
-def perfil():
-    # si hay un usuario en la session carga el perfil
-    if 'usuario' not in session:
-        return redirect('/')
-    # Query para obtener datos del perfil
-    else:
-        query_perfil = ('''
+def consultar_informacion_perfil(rut_perfil): # Query para consultar la informacion del perfil
+    query = ('''
             SELECT
                 id_credencial,
                 Credencial.nombre AS credencial_nombre,
@@ -72,11 +83,30 @@ def perfil():
             ON Credencial.id = Usuario.id_credencial
             WHERE rut = %s
         ''')
-        cursor.execute(query_perfil,(session['usuario']['rut'],))
-        resultado_perfil = cursor.fetchone()
-        resultado_perfil['rut'] = rut_chile.formato_rut(resultado_perfil['rut']) # Le da formato al rut como "12.345.678-9"
+    cursor.execute(query,(rut_perfil,))
+    resultado_perfil = cursor.fetchone()
+    return resultado_perfil
 
-        
+
+@mod.route('/debug_sol_query') # link para hacer debug de la query
+def debug_sol_query():
+    solicitudes = consultar_solicitudes_alumno("198182354") # Consulta las solicitudes a partir del rut
+    ids = get_id_from_list_of_dictionary(solicitudes) # Obtiene las id de las solicitudes
+    soliciturdes_equipos = consultar_equipos_solicitudes_alumno(ids)
+    for i in solicitudes:
+        print(i)
+    # # print(resultado)
+    return 'debug_sol_query'
+
+
+
+@mod.route('/perfil') # ** Importante PERFIL** #
+def perfil():
+    # si hay un usuario en la session carga el perfil
+    if 'usuario' not in session:
+        return redirect('/')
+    else:
+        resultado_perfil = consultar_informacion_perfil(session['usuario']['rut']) # Hace la consulta a la base sobre los datos del usuario
         validar_completar = False # Variable que sera pasada para completar la información
         # Loop para validar si es que faltan completar datos (marcados como None)
         # Si falta completar algún dato, la variable cambiará a True
@@ -84,8 +114,6 @@ def perfil():
             if resultado_perfil[key] == None:
                 validar_completar = True
                 break
-        
-        
         # Funcion que verifica si existe la foto de perfil
         # Las fotos puedes estar guardadas en cualquier tipo de extension
         # Si existe una foto para del usuario obtiene el nombre del archivo + extension
@@ -98,11 +126,14 @@ def perfil():
 
         # print(archivo_foto_perfil)         
         # print('resultado: ', resultado_perfil)
-
+        solicitudes = consultar_solicitudes_alumno(session['usuario']['rut']) # Consulta las solicitudes a partir del rut
+        ids = get_id_from_list_of_dictionary(solicitudes) # Obtiene las id de las solicitudes
+        solicitudes_equipos = consultar_equipos_solicitudes_alumno(ids) # Obtiene todas las solicitudes de los equipos
 
         return render_template(
             'vistas_perfil/perfil.html',
-            perfil_rut = session['usuario']['rut'],
+            solicitudes = solicitudes,
+            solicitudes_equipos = solicitudes_equipos,
             perfil_info = resultado_perfil,
             dir_foto_perfil = archivo_foto_perfil,
             completar_info = validar_completar)
