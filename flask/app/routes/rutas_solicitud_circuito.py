12f4dfdca88e53a223839a23b36f5e54fdc3a1ea
Weebtor <48831156+Vistorete@users.noreply.github.com>
2020-09-13 22:17:59 -0300

    Upadte del carrito

--- flask/app/routes/rutas_solicitud_circuito.py
+++ flask/app/routes/rutas_solicitud_circuito.py
@@ -23,12 +23,34 @@ def consultar_lista_circuito():
     cursor.execute(query)
     return cursor.fetchall()
 
-@mod.route("/gestion_solicitudes_prestamos_circuitos")
+
+# def consultar_carro_circuitos():
+#     if 'carro_circuito' not in session or len(session["carro_circuito"]) < 1:
+#         return []
+#     format_strings = ','.join(['%s'] * len(session["carro_circuito"]))
+#     list = []
+#     for element in session["carro_circuito"]:
+#         list.append(element)
+#     query = ('''
+#         SELECT id, nombre
+#         FROM Circuito
+#         WHERE id IN (%s)
+#     ''' % format_strings)
+#     cursor.execute(query,tuple(list))
+#     query_resultado = cursor.fetchall()
+#     print (query_resultado)
+#     return []
+    
+    
+@mod.route("/solicitudes_prestamos_circuitos")
 def gestion_solicitudes_prestamos_circuitos():
+    if 'carro_circuito' in session:
+        for element in session["carro_circuito"].items():
+            print(element)
     
     return render_template('solicitudes_prestamos_circuitos/preview.html',
-                           lista_circuitos = consultar_lista_circuito())
-    
+                           lista_circuitos = consultar_lista_circuito()                           )
+    # lista_carro_circuitos = session
 @mod.route("/debug_circuito")
 def debug_circuito():
     lista_circuitos = consultar_lista_circuito()
@@ -49,47 +71,42 @@ def generar_solicitud_alumno(rut_alumno, motivo): # funcion para generar la soli
     id_solicitud = cursor.lastrowid # Se obtiene el id de solicitud recién creada
     
     return
+@mod.route("/eliminar_carro_circuito",methods=['POST'])
+def eliminar_carro_circuito():
+    if request.method == "POST":
+        id_circuito = request.form["id_circuito"]
+        print(session["carro_circuito"][id_circuito])
+        del session["carro_circuito"][id_circuito]
+        if len(session["carro_circuito"]) == 0:
+            del session["carro_circuito"]
+        return render_template("solicitudes_prestamos_circuitos/tablas/lista_carro_circuito.html")
+    return jsonify({'error':'missing data!'})
+
+
 
 @mod.route("/agregar_al_carro_circuito",methods=['POST'])
 def agregar_al_carro_circuito():
     if request.method == "POST":
         id_circuito = request.form["id_circuito"]
         cantidad = request.form["cantidad"]
-        
+        nombre = request.form["nombre_circuito"]
         if id_circuito and cantidad:
             if 'carro_circuito' not in session: # si no hay carrito
                 session["carro_circuito"] = {}
             
-            if id_circuito not in session["carro_circuito"].keys(): # Si el circuito no esta
+            if id_circuito not in session["carro_circuito"].keys(): # Si el circuito no esta en el carro, lo agrega
                 session["carro_circuito"][id_circuito] = {} # Crea un diccionario carro = { id : {}}
                 session["carro_circuito"][id_circuito]['id'] = str(id_circuito) # carro = { id : {'id': 'id'}}
                 session["carro_circuito"][id_circuito]['cantidad'] = int(cantidad)  # carro = { id : {'id': str , 'cantidad' : int}}
-                print(session["carro_circuito"])            
-                return jsonify({id_circuito : {'id_circuito': id_circuito, 'cantidad' : cantidad }})
-            else:
-                session["carro_circuito"][id_circuito]['cantidad'] = int(session["carro_circuito"][id_circuito]['cantidad']) + int(cantidad) 
-                return jsonify({id_circuito : {'id_circuito': id_circuito, 'cantidad' : cantidad }})
-                print(session["carro_circuito"])    
-                # carro = { id : {'id': str , 'cantidad' : int + int}}
+                session["carro_circuito"][id_circuito]['nombre'] = nombre #agrega el nombre
+            else: # Si el circuito está en el carro, le suma la cantidad
+                session["carro_circuito"][id_circuito]['cantidad'] = int(cantidad)    
+            return render_template("solicitudes_prestamos_circuitos/tablas/lista_carro_circuito.html")
         else:
             return jsonify({'error':'missing data!'})
     
-    return None
+    return jsonify({'error':'missing data!'})
     
-    # print(datos_circuito_pedido)
-    # if "carro_pedidos_circuito" not in session.keys(): # crea la sesion
-    #     session["carro_pedidos_circuito"] = {}
-    
-    # if str(datos_circuito_pedido["id_circuito"]) in session["carro_pedidos_circuito"].keys(): # si ya hay le suma
-    #     session["carro_pedidos_circuito"][str(datos_circuito_pedido["id_circuito"])] = int(datos_circuito_pedido["cantidad_unidades_solicitadas"]) + int(session["carro_pedidos_circuito"][str(datos_circuito_pedido["id_circuito"])])
-    # else:
-    #     session["carro_pedidos_circuito"][str(datos_circuito_pedido["id_circuito"])] = int(datos_circuito_pedido["cantidad_unidades_solicitadas"]) # si no se crea y le agrega un valor
-    # print(session["carro_pedidos_circuito"])
-    
-    # return render_template("/solicitudes_prestamos_circuitos/tablas/lista_carro_circuito.html")
-
-    # return render_template("/solicitudes_prestamos/seccion_carro_pedidos.html",
-    #     lista_carro=lista_carro)
     
 @mod.route("/confirmar_al_carro_circuito",methods=['POST'])
 def confirmar_carro_circuito():
